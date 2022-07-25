201a15d5540b8cdf47142d5c92f1fd649cfe3673
= <=>
2022-07-24 22:06:16 -0500

    cleanup

--- src/app/ship/models/feature.ts
+++
@@ -1,6 +0,0 @@
-export interface Feature {
-    id: number;
-    id_ship: number;
-    id_feature: number;
-    value_feature: string;
-}
--- src/app/ship/models/ship.ts
+++
@@ -1,7 +0,0 @@
-export interface Ship {
-    id: number;
-    name: string;
-    date_creation: string;
-    date_destruction: string;
-    id_type: number;
-}
